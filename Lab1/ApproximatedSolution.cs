011b0a6bf900ce0362e4744844c5076d661ef8b0
vladislav.afletunov <onverx@gmail.com>
2022-09-30 13:29:22 +0500

    Конструктор + рандом

---
+++ Lab1/ApproximatedSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MathPrimitiveLibrary;
+
+namespace AFLETUNOV_LR1.Lab1
+{
+  public class ApproximatedSolution
+  {
+    public Matrix InitMatrix { get; private set; }
+
+    public Matrix ExpandedMatrix { get; private set; }
+
+    public double randomValue { get; }
+    public Random r { get; private set; }
+
+    public ApproximatedSolution(int size)
+    {
+      InitMatrix = new Matrix(size);
+      ExpandedMatrix = new Matrix(size + 2, size);
+      r = new Random();
+      randomValue = (r.NextDouble() - 0.5) * 2;
+    }
+  }
+}
