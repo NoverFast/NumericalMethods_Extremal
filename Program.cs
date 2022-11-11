83cf29c41964a3489c20c541a9b1fda318647169
Nover <onverx@gmail.com>
2022-11-11 19:34:07 +0500

    Отладочные функции для счёта интегралов разными методами.

--- Program.cs
+++ Program.cs
@@ -10,12 +10,17 @@ namespace ExtremalOptimization
     static void Main(string[] args)
     {
       //ApproximatedSolution aprx = new ApproximatedSolution(10);
-      RegularMesh rMesh = new RegularMesh(0, 1, 1);
+      RegularMesh rMesh = new RegularMesh(0, 5, 100);
       rMesh.PrintMeshProperties();
       Rectangle rectangle = new Rectangle(rMesh, (x) => x*x);
+      Simpson simpson = new Simpson(rMesh, (x) => x * x);
+      Trapezoid trap = new Trapezoid(rMesh, (x) => x * x);
       Console.WriteLine(rectangle.Left());
       Console.WriteLine(rectangle.Right());
       Console.WriteLine(rectangle.Middle());
+      Console.WriteLine(trap.Calculate());
+   //   Console.WriteLine(simpson.Calculate());
+      //rMesh.PrintMeshData();
       Console.ReadLine();
     }
   }
